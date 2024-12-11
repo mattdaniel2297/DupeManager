955eb84bb5d956a29407ad1ac290ab5dc565ffc7
Matt Daniel <matt@xps.com>
2024-12-11 20:24:19 +0100

    bug fix when db new

--- test.py
+++ test.py
@@ -5,11 +5,8 @@ from db_utils import create_db, catalog_items
 
 def test(root_dir):
     db_loc = os.path.join(root_dir, "database.db")
-    if os.path.exists(db_loc):
-        print("database exists")
-        catalog_items(root_dir, db_loc)
-    else:
+    if not os.path.exists(db_loc):
         create_db(db_loc)
-    # print(db_loc)
+    catalog_items(root_dir, db_loc)
         
 
